02f699c1ecf43de504fa8208c024be70f9edd0f8
dounmogni <dounmogni@yahoo.fr>
2023-03-31 16:02:35 +0200

    init

---
+++ tp-2/ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT YOUR OWN"
+  secret_key = "PUT YOUR OWN"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-012cc038cc685a0d7"
+  instance_type = "t2.micro"
+  key_name      = "devops-nicolas"
+  tags = {
+    Name = "ec2-nicolas"
+ 
+  }
+ root_block_device {
+   delete_on_termination = true
+  }
+}
