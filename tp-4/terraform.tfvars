0d55a652c0a27056e4f1990b374dbaaaa8d56444
dounmogni <dounmogni@yahoo.fr>
2023-03-31 16:05:37 +0200

    ajout tp4

---
+++ tp-4/terraform.tfvars
@@ -0,0 +1,8 @@
+instancetype = "t2.micro"
+
+aws_common_tag = {
+  name : "ec2-Nicolas"
+}
+
+AWS_ACCESS_KEY = "PUT YOUR OWN"
+AWS_SECRET_KEY = "PUT YOUR OWN"
---
+++ tp-4/variable.tf
@@ -0,0 +1,18 @@
+variable "instancetype" {
+  type        = string
+  description = "set aws instance type"
+  default     = "t2.nano"
+
+}
+
+variable "aws_common_tag" {
+  type        = map(any)
+  description = "set aws tag"
+  default = {
+    Name = "ec2-DND"
+  }
+
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
