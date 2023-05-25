a4ac8b9fe0ad7860ba10995aaf42bc42ef9e5966
dounmogni <dounmogni@yahoo.fr>
2023-05-26 00:05:21 +0200

    ajout tp-v2

---
+++ tp-6_v2/dev/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "us-east-1"
+  #access_key = var.AWS_ACCESS_KEY
+  #secret_key = var.AWS_SECRET_KEY
+  #shared_credentials_file = "~/.aws/credentials" #lecture de vos credentials en local.(aws cli/aws configure)
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-nicolas"
+    key    = "nicolas-dev.tfstate"
+    region = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+    #access_key = var.AWS_ACCESS_KEY
+    #secret_key = var.AWS_SECRET_KEY
+  }
+}
+
+module "ec2" {
+  source = "../modules/ec2module"
+  instancetype = "t2.nano"
+ # instancetype = var.instancetype
+  aws_common_tag = {
+    Name = "ec2-dev-Nicolas"
+  }
+ 
+  
+  sg_name = "dev-Nicolas-sg"
+}
